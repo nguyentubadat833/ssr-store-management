37665e578a63dd4b5631a129a72f271c80e20c3f
nguyentubadat <nguyentubadat833@gmail.com>
2024-10-25 16:40:12 +0700

    update

--- server/api/receiving/delete.delete.ts
+++ server/api/receiving/delete.delete.ts
@@ -1,7 +1,37 @@
 import {IReceivingDeleteReq} from "~/types/IReceiving";
+import purchaseOrderError from "~/server/utils/error/purchaseOrderError";
+import receivingError from "~/server/utils/error/receivingError";
 
 export default defineEventHandler(async (event) => {
-    const prams: IReceivingDeleteReq = getQuery(event)
+    const params: IReceivingDeleteReq = getQuery(event)
+    const status = await prismaClient.receiving.findUniqueOrThrow({
+        where: {
+            code: params.receivingCode
+        },
+        select: {
+            status: true
+        }
+    })
+        .then(result => {
+            return result.status
+        })
+        .catch((error) => {
+            return handlerError(error, event)
+        })
 
-
+    if (status === 2) {
+        return receivingError.imported(event)
+    } else {
+        await prismaClient.stock.deleteMany({
+            where: {
+                receivingCode: params.receivingCode
+            }
+        })
+        await prismaClient.receiving.delete({
+            where: {
+                code: params.receivingCode
+            }
+        })
+        setResponseStatus(event, 204)
+    }
 })
